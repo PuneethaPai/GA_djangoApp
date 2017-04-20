951d215af2b74bb25c22ef978f9624f628b436de
Puneetha Pai B P <puneethp@thoughtworks.com>
2017-04-20 13:21:10 +0530

    A simple template rendering to display list of numbers

--- Genetic_Algorithm/views.py
+++ Genetic_Algorithm/views.py
@@ -1,4 +1,12 @@
+import random
+
 from django.http import HttpResponse
+from django.template import loader
+
 
 def index(request):
-    return HttpResponse("Hello World")
+    template = loader.get_template('templates/index.html')
+    context = {
+        'list': random.sample(xrange(100), 10)
+    }
+    return HttpResponse(template.render(context, request))
