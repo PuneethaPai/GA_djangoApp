5b8ad34062b89eb07237724a3ee369eb76b4a9ec
Puneetha Pai B P <puneethp@thoughtworks.com>
2017-05-03 18:38:00 +0530

    Redirect frunction after submiting the form

--- web/read.js
+++ web/read.js
@@ -1,10 +1,19 @@
 $(document).ready(function(){
-    var form = $('#form');
-    form.submit(helloThere);
+    $form = createForm();
+    $form.submit(redirect);
+    $('body').append($form);
     console.log("Yes I have been included");
 });
 
-function helloThere(){
-    input = $('#text').val()
-    $('body').html(input);
+function createForm(){
+    $form = $("<form></form>");
+    $form.append('<input type="text" id = "text" placeholder="Anything">');
+    $form.append('<input type="submit" value="Submit">');
+    return $form
+}
+
+function redirect(){
+    input = $('#text').val();
+    $('body').html('')
+    window.location='http://127.0.0.1:8000';
 }
