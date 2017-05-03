e41095cfb473e8d560c1f94f660442779831fa43
Puneetha Pai B P <puneethp@thoughtworks.com>
2017-05-03 17:35:39 +0530

    Seperate out the web and backend part

--- web/read.js
+++ web/read.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
+    var form = $('#form');
+    form.submit(helloThere);
     console.log("Yes I have been included");
 });
+
+function helloThere(){
+    input = $('#text').val()
+    $('body').html(input);
+}
