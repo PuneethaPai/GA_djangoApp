91afe3fd187acd56d9b78f0d47dc1670c11549aa
Puneetha Pai B P <puneethp@thoughtworks.com>
2017-05-05 12:07:31 +0530

    Implemented form functionality using normal Button

--- web/read.js
+++ web/read.js
@@ -1,19 +1,16 @@
-$(document).ready(function(){
-    $form = createForm();
-    $form.submit(redirect);
-    $('body').append($form);
-    console.log("Yes I have been included");
-});
-
-function createForm(){
-    $form = $("<form></form>");
-    $form.append('<input type="text" id = "text" placeholder="Anything">');
-    $form.append('<input type="submit" value="Submit">');
-    return $form
-}
-
 function redirect(){
     input = $('#text').val();
-    $('body').html('')
-    window.location='http://127.0.0.1:8000';
+    fetch('http://127.0.0.1:8000/')
+    .then(function(result){
+        return result.json()
+    })
+    .then(function(jsonResult){
+        $('h1').remove();
+        $('#form').remove();
+        $('body').append((jsonResult.list).toString())
+        console.log(jsonResult.list);
+    })
+    .catch(function(error){
+        console.log(error);
+    })
 }
