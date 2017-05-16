c6dfa59abd83e9d0f1210152163d9bdb5d917b5a
Puneetha Pai B P <puneethp@thoughtworks.com>
2017-05-16 15:12:12 +0530

    Request post from fetch api is failing to read JSON response

--- web/read.js
+++ web/read.js
@@ -1,13 +1,21 @@
 function redirect(){
-    input = $('#text').val();
-    fetch('http://puneeth.pythonanywhere.com/')
+    input = $('input:input').val()
+    if(input === ""){
+        return
+    }
+    data = new FormData()
+    data.append('input', input)
+    fetch('http://127.0.0.1:8000/run/',
+    {
+        method : "POST",
+        body : data
+    }
+    )
     .then(function(result){
+        console.log(result.)
         return result.json()
     })
     .then(function(jsonResult){
-        $('h1').remove();
-        $('#form').remove();
-        $('body').append((jsonResult.list).toString())
         console.log(jsonResult.list);
     })
     .catch(function(error){
